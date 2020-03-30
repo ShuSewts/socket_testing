e6ce73de3eff21d2228652803b840ab974fad205
Shu <shu.lo@sewts.de>
2020-03-30 17:20:02 +0200

    testing so far

---
+++ src/socket_client.py
@@ -0,0 +1,56 @@
+
+# Import socket module
+import socket
+import rospy
+import threading
+
+class FakeClient:
+    def __init__(self, port):
+        rospy.init_node("fake_client", anonymous=True)
+        # Create a socket object
+        self.s = socket.socket()
+
+        # Define the port on which you want to connect
+        self.port = port
+
+        # connect to the server on local computer
+        self.s.connect(('127.0.0.1', self.port))
+        self.buffer = 128
+        self.kill = False
+
+        # receive data from the server
+        print self.s.recv(1024)
+        t1 = threading.Thread(target=self.heartbeat, kwargs={})
+        #t1.start()
+
+    def send(self, data): #should be if the length of the data is less than or equal to 2 bytes send it
+        totalsent = 0
+        while totalsent < len(data):
+            sent = self.s.send(data[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+        print("message sent")
+
+    def receive(self):
+        return len(self.s.recv(self.buffer)) > 0
+
+    def close(self):
+        self.s.close()
+
+    def heartbeat(self):
+        while not self.kill:
+            self.send("HIGH")
+            if not self.receive():
+                self.kill = True
+            rospy.sleep(0.5)
+            self.send("LOW")
+            if not self.receive():
+                self.kill = True
+            rospy.sleep(0.5)
+
+if __name__ == "__main__":
+    F = FakeClient(12345)
+    F.send("afaf")
+    F.receive()
+    F.close()
